2f0a72cc00f920d194ceb6da80326410507145bd
Josh Datko <jbdatko@gmail.com>
2014-12-11 07:42:26 -0700

    Sunny day working.

--- simon/firmware/simon.ino
+++ simon/firmware/simon.ino
@@ -33,96 +33,6 @@
  * Public Constants
  *************************************************/
 
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
-#define NOTE_A1  55
-#define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
-#define NOTE_CS6 1109
-#define NOTE_D6  1175
-#define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
-#define NOTE_CS7 2217
-#define NOTE_D7  2349
-#define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
-#define NOTE_FS7 2960
-#define NOTE_G7  3136
-#define NOTE_GS7 3322
-#define NOTE_A7  3520
-#define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
-#define NOTE_CS8 4435
-#define NOTE_D8  4699
-#define NOTE_DS8 4978
-
 
 
 //***********************************************************
@@ -259,102 +169,101 @@ static uint8_t rsp[32] = {0};
 
 AtSha204 sha = AtSha204();
 
-void testMac(void)
-{
-    to_mac[0] = 1;
+// void testMac(void)
+// {
+//     to_mac[0] = 1;
 
-    if (0 == sha.macBasic(to_mac, sizeof(to_mac)))
-    {
-        for (int x = 0; x < sha.rsp.getLength(); x++)
-            Serial.write(*(sha.rsp.getPointer() + x));
-        //sha.rsp.dumpHex(&Serial);
+//     if (0 == sha.macBasic(to_mac, sizeof(to_mac)))
+//     {
+//         for (int x = 0; x < sha.rsp.getLength(); x++)
+//             Serial.write(*(sha.rsp.getPointer() + x));
+//         //sha.rsp.dumpHex(&Serial);
 
-        if (0 == sha.checkMacBasic(to_mac, sizeof(to_mac), const_cast<uint8_t*>(sha.rsp.getPointer())))
-            Serial.print('*');
-    }
-    else
-    {
-        Serial.println("FAIL");
-    }
-}
-void getRandom(void)
-{
-    if (0 == sha.getRandom()){
-        Serial.println("Success");
-        sha.rsp.dumpHex(&Serial);
-    }
-    else{
-        Serial.println("Failue");
-    }
+//         if (0 == sha.checkMacBasic(to_mac, sizeof(to_mac), const_cast<uint8_t*>(sha.rsp.getPointer())))
+//             Serial.print('*');
+//     }
+//     else
+//     {
+//         Serial.println("FAIL");
+//     }
+// }
 
-}
-void wakka(void) {
+// void getRandom(void)
+// {
+//     if (0 == sha.getRandom()){
+//         Serial.println("Success");
+//         sha.rsp.dumpHex(&Serial);
+//     }
+//     else{
+//         Serial.println("Failue");
+//     }
 
-  Wire.begin(); // join i2c bus (address optional for master)
+// }
+// void wakka(void) {
 
-  Wire.beginTransmission(addr); // transmit to device #4
-  for (int x = 0; x < sizeof(wakeup); x++)
-  {
-    Wire.write(wakeup[x]);
-  }
-  Wire.endTransmission();    // stop transmitting
+//   Wire.begin(); // join i2c bus (address optional for master)
 
-  delay(2.5);
+//   Wire.beginTransmission(addr); // transmit to device #4
+//   for (int x = 0; x < sizeof(wakeup); x++)
+//   {
+//     Wire.write(wakeup[x]);
+//   }
+//   Wire.endTransmission();    // stop transmitting
 
-  Wire.requestFrom(addr, 4);    // request 6 bytes from slave device #2
+//   delay(2.5);
 
-  while(Wire.available())    // slave may send less than requested
-  {
-    char c = Wire.read();
+//   Wire.requestFrom(addr, 4);    // request 6 bytes from slave device #2
 
-  }
+//   while(Wire.available())    // slave may send less than requested
+//   {
+//     char c = Wire.read();
 
-  Wire.beginTransmission(addr); // transmit to device #4
-  for (int x = 0; x < sizeof(buf); x++)
-  {
-    Wire.write(buf[x]);
-  }
-  Wire.endTransmission();    // stop transmitting
+//   }
 
-  delay(40);
+//   Wire.beginTransmission(addr); // transmit to device #4
+//   for (int x = 0; x < sizeof(buf); x++)
+//   {
+//     Wire.write(buf[x]);
+//   }
+//   Wire.endTransmission();    // stop transmitting
 
-  Wire.requestFrom(addr, 32);    // request 6 bytes from slave device #2
+//   delay(40);
 
-  while(Wire.available())    // slave may send less than requested
-  {
-    char c = Wire.read(); // receive a byte as character
-    Serial.print(c, HEX);
-  }
+//   Wire.requestFrom(addr, 32);    // request 6 bytes from slave device #2
 
-  Serial.print('\n');
-}
+//   while(Wire.available())    // slave may send less than requested
+//   {
+//     char c = Wire.read(); // receive a byte as character
+//     Serial.print(c, HEX);
+//   }
+
+//   Serial.print('\n');
+// }
 
 void loop()
 {
   play_winner(); // Intro sequence for startup
 
-  testMac();
+  // Wait in attract mode until a button is pressed
+  // attract mode will return 1 if this player is the first
+  // player to press a button
+  first_turn = attract_mode();
 
+  // Begin game: flash all LEDs, then go into game mode
+  set_leds(LED1_MASK | LED2_MASK | LED3_MASK | LED4_MASK);
   delay(1000);
+  set_leds(0);
+  delay(250);
 
-  // // Wait in attract mode until a button is pressed
-  //       // attract mode will return 1 if this player is the first
-  //       // player to press a button
-  //       first_turn = attract_mode();
+  // Play game and handle result
+  // game_mode() function returns 1 for win, 0 for loss
+  if(game_mode()) {
+      play_winner();
+  }
 
-  // // Begin game: flash all LEDs, then go into game mode
-  // set_leds(LED1_MASK | LED2_MASK | LED3_MASK | LED4_MASK);
-  // delay(1000);
-  // set_leds(0);
-  // delay(250);
-
-  // // Play game and handle result
-  //       // game_mode() function returns 1 for win, 0 for loss
-  // if(game_mode()) {
-  //     play_winner();
-  // }
-
-  // else { play_loser(); }
+  else {
+      play_loser();
+  }
 }
 
 
@@ -497,14 +406,32 @@ byte receive_move(void)
     if(Serial.available())
     {
       choice = Serial.read(); // Read in the sent character
-      if(choice == 'l') { return 0; } // Other player has lost, return 0
+      if (choice == 'l')
+      {
+          // Other player has lost, return 0
+          return 0;
+      }
       else { // Sent char was a button press
         choice = choice - 48; // Convert char from ASCII to button value
         moves[nmoves++] = choice;  // Add to list of moves
 
-        for (int x=0; x < sizeof(rsp); x++)
-            rsp[x] = Serial.read();
+        int x = 0;
+        while (x < sizeof(rsp))
+        {
+            if (Serial.available())
+            {
+                rsp[x] = Serial.read();
+                x++;
+            }
+        }
 
+        to_mac[0] = choice;
+
+        if (0 != sha.checkMacBasic(to_mac, sizeof(to_mac), rsp))
+        {
+            Serial.print('l');
+            return 0;
+        }
 
         return 1;
       }
@@ -587,8 +514,12 @@ byte attract_mode(void)
     if(Serial.available())
     {
       received = Serial.read();
-      if(received == 'c') { return 0; } // Return 2 if we receive a 'c'hallenge from another player
-                                        // This means player is player 2
+      if(received == 'c')
+      {
+          // Return 2 if we receive a 'c'hallenge from another player
+          // This means player is player 2
+          return 0;
+      }
     }
     current_led = (current_led << 1) % 0x0F; // bit shift 1 > 2 > 4 > 8 > 1 > 2...
   }
